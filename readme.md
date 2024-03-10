b852198b09884d2621f3e32899e9d9e115ced4ba
EMI\migli <emigliorata@gmail.com>
2024-03-10 11:48:08 -0300

    Added get new token address from tx initialize2

--- readme.md
+++ readme.md
@@ -3,3 +3,18 @@
 ## Initialize Pool
 
 ![Initialize Pool](docs/Initialize_pool.png)
+
+# Links 
+https://anchor-lang.com/docs/intro-to-solana
+https://kevinheavey.github.io/anchorpy/
+https://kevinheavey.github.io/solders/tutorials/index.html
+
+
+## Contrast with Ethereum
+https://solana.wiki/zh-cn/docs/ethereum-comparison/
+
+## Explaination of accounts
+https://medium.com/@jorge_londono_31005/understanding-solanas-mint-account-and-token-accounts-546c0590e8e
+
+https://medium.com/coinmonks/how-to-become-a-solana-blockchain-developer-in-2022-cfc38f1370b9
+https://github.com/SolanaNatives/Solana-Programming-Resources
--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,17 @@ import express from "express";
 import { TOKEN_PROGRAM_ID, unpackAccount } from "@solana/spl-token";
 import { Connection, PublicKey, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { urlToHttpOptions } from "url";
+import {
+  LiquidityPoolKeysV4,
+  MARKET_STATE_LAYOUT_V3,
+  Market,
+} from "@raydium-io/raydium-sdk";
 
-const sleep = (ms: number) => {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-};
-
-const WSS_ENDPOINT =
-  "https://mainnet.helius-rpc.com/?api-key=";
+const WSS_ENDPOINT = "https://red-divine-flower.solana-mainnet.quiknode.pro//";
 const HTTP_ENDPOINT =
   "https://red-divine-flower.solana-mainnet.quiknode.pro/fix/";
 
-const solanaConnection = new Connection(HTTP_ENDPOINT, {
-  wsEndpoint: WSS_ENDPOINT,
-});
+const RAYDIUM_PUBLIC_KEY = "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8"; // "5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1" Raydium Authority V4
 
 const app = express();
 const port = 3000;
@@ -30,7 +28,7 @@ app.listen(port, () => {
 
 (async () => {
   const publicKey = new PublicKey(
-    "5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1" //Raydium Authority V4
+    RAYDIUM_PUBLIC_KEY //Raydium Authority V4
   );
 
   const solanaConnection = new Connection(WSS_ENDPOINT, {
@@ -60,6 +58,48 @@ app.listen(port, () => {
     },
     "confirmed"
   ); */
+  function generateExplorerUrl(txId) {
+    return `https://solscan.io/tx/${txId}?cluster=mainnet`;
+  }
+
+  const getAccount = async (address: string, connection) => {
+    const publicKey = new PublicKey(address);
+    const accountInfo = await connection.getAccountInfo(publicKey);
+    // return accountInfo?.data;
+    if (accountInfo && accountInfo.data) {
+      return MARKET_STATE_LAYOUT_V3.decode(accountInfo.data);
+    }
+  };
+
+  async function fetchRaydiumAccounts(signature, connection) {
+    const txId = signature;
+    const tx = await connection.getParsedTransaction(txId, {
+      maxSupportedTransactionVersion: 0,
+      commitment: "confirmed",
+    });
+    const accounts = tx?.transaction?.message?.instructions.find(
+      (ix) => ix.programId.toBase58() === RAYDIUM_PUBLIC_KEY
+    ).accounts;
+
+    if (!accounts) {
+      console.log("No accounts found");
+      return;
+    }
+    const tokenAIndex = 8;
+    const tokenBIndex = 9;
+
+    const tokeAAccount = accounts[tokenAIndex];
+    const tokenBAccount = accounts[tokenBIndex];
+    const displayData = [
+      { Token: "Token A", account: tokeAAccount },
+      { Token: "Token B", account: tokenBAccount },
+    ];
+    console.log("New Raydium  Liquidity Pool Created Found");
+    console.log(generateExplorerUrl(txId));
+    console.table(displayData);
+    //console.log(await getAccount(tokeAAccount, connection));
+    // await sleep(2000);
+  }
 
   //Listening for logs on the readyus addrr and looking for pool initialization txs
   solanaConnection.onLogs(
@@ -68,7 +108,8 @@ app.listen(port, () => {
       if (err) return;
 
       if (logs && logs.some((log) => log.includes("initialize2"))) {
-        console.log({ signature }, JSON.stringify(logs));
+        console.log({ signature });
+        fetchRaydiumAccounts(signature, solanaConnection);
         //with the signature get the token created(seems not to be in the logs :/ ), and get more info about it (market cap, liq, holders)
         //Examples to follow: https://github.com/raydium-io/raydium-sdk-V1-demo/blob/master/src/subNewAmmPool.ts
         //https://github.com/rpcpool/yellowstone-grpc/blob/master/examples/typescript/src/client.ts
